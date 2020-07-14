df35febd666789d6ff17d38e1cb32e3e1c7fa6a7
Turetska <oturetska@yahoo.com>
2020-07-14 19:11:59 -0400

    added first promt, determined functions and start the first function

--- index.js
+++ index.js
@@ -10,15 +10,67 @@ const connection = mysql.createConnection({
     port: 3306
 });
 
+// connect to the mysql database
 connection.connect((err) => {
     if (err) throw err;
-
-    startApp();
+    // run the start function after connection is made
+    start();
 });
 
-function startApp() {
-    inquirer.prompt ()
+// promting user to choose:
+function start() {
+    inquirer.prompt({
+        name: "options",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "- Viev All Employees",
+            "- Viev All Employees by Department",
+            "- Viev All Employees by Role",
+            "- Add Employee",
+            "- Add Role",
+            "- Add Department",
+            "- Update Employee Role",
+            "- Exit"
+        ]
+
+    })
+        .then(answers => {
+            // Create code that creates a new employee object based off the employee class and pushes it to the teamArray.
+            switch (answers.options) {
+                case "- Viev All Employees":
+                    viewEmployees();
+                    break;
+                case "- Viev All Employees by Department":
+                    viewByDepartm();
+                    break;
+                case "- Viev All Employees by Role":
+                    viewByRole();
+                    break;
+                case "- Add Employee":
+                    addEmployes();
+                    break;
+                case "- Add Role":
+                    addRole();
+                    break;
+                case "- Add Department":
+                    addDept();
+                    break;
+                case "- Update Employee Role":
+                    updateRole();
+                    break;
+                case "- Exit":
+                    connection.end();
+                    break;
+            };
+        })
 }
 
-
-connection.end();
+// Function to view all Employees
+function viewEmployees() {
+connection.query("SELECT * from employee", (err, results) => {
+    if (err) throw err;
+    console.table(results);
+    start();
+})
+}
